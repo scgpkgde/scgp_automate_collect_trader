085437b45b341772bcf74ec9957c207cc4004561
cheetahctss <36398388+CheetahCTSS@users.noreply.github.com>
2026-01-12 14:11:41 +0700

    add setting

---
+++ lib/setting.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+import pandas as pd
+
+SETTINGS = {
+    'CHROMEDRIVER_PATH': '/Users/samapatsrihan/work/scgp/scgp_ir/driver/chromedriver',
+    'user': 'krittate@scg.com',
+    'password':'@Khwan01',
+    'hs_code': ["48191000","48192000"]
+
+}
